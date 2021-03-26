526921d9a719067a4918f9024f9da10be530be31
pedrohcalado <pedrocalado22@gmail.com>
2021-03-26 15:34:06 -0300

    SRP

---
+++ Solid/Solid/Program.cs
@@ -0,0 +1,17 @@
+﻿using Solid.SRP;
+using Solid.SRP.Repository;
+using System;
+
+namespace Solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fornecedor forn = new Fornecedor();
+            forn.ChangeName("Pedro");
+            IFornecedorRepository rep = new FornecedorRepository();
+            rep.Salvar(forn);
+        }
+    }
+}
---
+++ Solid/Solid/SRP/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solid.SRP.Services;
+
+namespace Solid.SRP
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void ChangeName (string name)
+        {
+            this.Name = name;
+            // Disparar evento para informar o RH sobre mudança de nome (isso não deve ficar como responsabilidade do fornecedor)
+            NotificationService.Notify("rg@gmail.com");
+        }
+       
+
+    }
+}
